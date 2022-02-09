566b3e42be6901a854e2332f9e0d8e53e3010b0b
Daniel Hudson <35522603+DanielHudson2@users.noreply.github.com>
2022-02-09 21:46:21 +0000

    started header

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -3,48 +3,41 @@
     $blog_info    = get_bloginfo( 'name' );
     $description  = get_bloginfo( 'description', 'display' );
     $show_title   = ( true === get_theme_mod( 'display_title_and_tagline', true ) );
-    $header_class = $show_title ? 'site-title' : 'screen-reader-text';
+    $header_class = $show_title ? 'c-site-branding__title' : 'screen-reader-text';
 
-    if ( has_custom_logo() && $show_title ) : 
+?><div class="c-site-branding"><?php 
     
-        ?><div class="site-logo"><?php the_custom_logo(); ?></div><?php 
-
-    endif; 
-
-?><div class="site-branding"><?php 
-
-    if ( has_custom_logo() && ! $show_title ) : 
+    ?><div class="c-site-branding__logo"><?php the_custom_logo(); ?></div>
     
-        ?><div class="site-logo"><?php the_custom_logo(); ?></div><?php 
+    <div class="c-site-branding__info"><?php 
+    
+        if ( $blog_info ) : 
         
-    endif;
-    
-    if ( $blog_info ) : 
-    
-        if ( is_front_page() && ! is_paged() ) : 
-        
-            ?><h1 class="<?php echo esc_attr( $header_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1><?php 
+            if ( is_front_page() && ! is_paged() ) : 
             
-        elseif ( is_front_page() && ! is_home() ) : 
-        
-            ?><h1 class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></h1><?php 
+                ?><h1 class="<?php echo esc_attr( $header_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1><?php 
+                
+            elseif ( is_front_page() && ! is_home() ) : 
             
-        else : 
+                ?><h1 class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></h1><?php 
+                
+            else : 
 
-            ?><p class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></p><?php
+                ?><p class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></p><?php
+                
+            endif; 
+                
+        endif; 
+        
+        if ( $description && true === get_theme_mod( 'display_title_and_tagline', true ) ) : 
+        
+            ?><p class="c-site-branding__description"><?php 
+            
+                echo $description; // phpcs:ignore WordPress.Security.EscapeOutput 
+                
+            ?></p><?php 
             
         endif; 
-            
-    endif; 
-    
-    if ( $description && true === get_theme_mod( 'display_title_and_tagline', true ) ) : 
-    
-        ?><p class="site-description"><?php 
-        
-            echo $description; // phpcs:ignore WordPress.Security.EscapeOutput 
-            
-        ?></p><?php 
-        
-    endif; 
 
-?></div><!-- .site-branding -->
+    ?></div>
+</div><!-- .site-branding -->
--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -1,6 +1,6 @@
 <?php
 
-    $wrapper_classes = 'site-header';
+    $wrapper_classes = 'c-site-header row columns';
     $wrapper_classes .= has_custom_logo() ? ' has-logo' : '';
     $wrapper_classes .= ( true === get_theme_mod( 'display_title_and_tagline', true ) ) ? ' has-title-and-tagline' : '';
     $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
